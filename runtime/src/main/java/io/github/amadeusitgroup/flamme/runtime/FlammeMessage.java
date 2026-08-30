f57276f7a08c5cd83e6a4aa36efc165d406f2c56
Mouhamad Al Mounayar <sonofthecometfinn@gmail.com>
2026-08-30 18:38:55 +0200

    feat: publish error reply when a component fails
    Signed-off-by: sonofthecomet-ctrl <sonofthecometfinn@gmail.com>

--- runtime/src/main/java/io/github/amadeusitgroup/flamme/runtime/FlammeMessage.java
+++ runtime/src/main/java/io/github/amadeusitgroup/flamme/runtime/FlammeMessage.java
@@ -3,8 +3,14 @@ package io.github.amadeusitgroup.flamme.runtime;
 import com.google.protobuf.Any;
 import java.util.Map;
 
-public record FlammeMessage(Any payload, Map<String, String> headers, String replyTo) {
+public record FlammeMessage(FlammeEnvelope envelope, Map<String, String> headers, String replyTo) {
   FlammeMessage wrap(Any payload) {
-    return new FlammeMessage(payload, this.headers(), this.replyTo());
+    FlammeEnvelope envelope = FlammeEnvelope.newBuilder().setPayload(payload).build();
+    return new FlammeMessage(envelope, this.headers(), this.replyTo());
+  }
+
+  FlammeMessage error(FlammeError error) {
+    FlammeEnvelope envelope = FlammeEnvelope.newBuilder().setError(error).build();
+    return new FlammeMessage(envelope, headers, replyTo);
   }
 }
--- runtime/src/main/java/io/github/amadeusitgroup/flamme/runtime/payload/MultiPayloadCodec.java
+++ runtime/src/main/java/io/github/amadeusitgroup/flamme/runtime/payload/MultiPayloadCodec.java
@@ -1,36 +1,23 @@
 package io.github.amadeusitgroup.flamme.runtime.payload;
 
-import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
 import io.github.amadeusitgroup.flamme.runtime.FlammeEnvelope;
-import io.github.amadeusitgroup.flamme.runtime.FlammeError;
 import io.github.amadeusitgroup.flamme.runtime.errors.FlammeImplRuntimeError;
 import io.github.amadeusitgroup.flamme.runtime.utils.Strings;
 import jakarta.enterprise.context.ApplicationScoped;
-import java.util.Optional;
 
 @ApplicationScoped
 public class MultiPayloadCodec implements PayloadCodec {
 
   @Override
-  public byte[] encodePayload(Any payload) {
-    return FlammeEnvelope.newBuilder().setPayload(payload).build().toByteArray();
+  public byte[] encodePayload(FlammeEnvelope envelope) {
+    return envelope.toByteArray();
   }
 
   @Override
-  public Any decodePayload(byte[] data) throws FlammeImplRuntimeError {
+  public FlammeEnvelope decodePayload(byte[] data) throws FlammeImplRuntimeError {
     try {
-      return FlammeEnvelope.parseFrom(data).getPayload();
-    } catch (InvalidProtocolBufferException e) {
-      throw new FlammeImplRuntimeError(Strings.ERROR_DECODING_REMOTE_PAYLOAD, e);
-    }
-  }
-
-  @Override
-  public Optional<FlammeError> decodeError(byte[] data) throws FlammeImplRuntimeError {
-    try {
-      var envelope = FlammeEnvelope.parseFrom(data);
-      return envelope.hasError() ? Optional.of(envelope.getError()) : Optional.empty();
+      return FlammeEnvelope.parseFrom(data);
     } catch (InvalidProtocolBufferException e) {
       throw new FlammeImplRuntimeError(Strings.ERROR_DECODING_REMOTE_PAYLOAD, e);
     }
